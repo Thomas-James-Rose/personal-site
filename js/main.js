294b5eef34a8d3a978f95b54a23611bd1d9281df
Tom Rose <Tom Rose>
2016-12-18 17:19:18 +0000

    Added parallax scroll, top nav bar, contact content and copyright notice

--- js/main.js
+++ js/main.js
@@ -1,1 +1,16 @@
-// nothing to see here yet...
+// on scroll
+$(window).on('scroll',function(){
+
+    var mainbottom = $('#page-header').offset().top + $('#page-header').height();
+
+    // we round here to reduce a little workload
+    stop = Math.round($(window).scrollTop());
+    if (stop > mainbottom) {
+        $('#main-nav').addClass('topnav');
+        $('#top-arrow').addClass('top-arrow-active')
+    } else {
+        $('#main-nav').removeClass('topnav');
+        $('#top-arrow').removeClass('top-arrow-active')
+    }
+
+});
