f0b65bbde68cde52270b406827f6261cf8b90cab
Tom Rose <Tom Rose>
2016-12-19 16:21:57 +0000

    Added mailto link and JS email obfuscation

--- js/main.js
+++ js/main.js
@@ -1,11 +1,11 @@
 // on scroll (source: http://stackoverflow.com/questions/28392145/change-css-after-scrolling)
 $(window).on('scroll',function(){
 
-    var mainbottom = $('#page-header').offset().top + $('#page-header').height();
+    var headerbottom = $('#page-header').offset().top + $('#page-header').height();
 
     // we round here to reduce a little workload
     stop = Math.round($(window).scrollTop());
-    if (stop > mainbottom) {
+    if (stop > headerbottom) {
         $('#main-nav').addClass('topnav');
         $('#top-arrow').addClass('top-arrow-active')
     } else {
