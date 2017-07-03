3777967a42d89a3033333a2c3d5214178e3ca225
Tom Rose <Tom Rose>
2017-07-03 14:26:53 +0100

    Updated README file and made some minor content updates

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # platform22-site
-A basic website for the band known as Platform 22
+A website for the band known as Platform 22 built from scratch and written purely in front-end languages.
+
+## GitHub Pages
+[Click here!](https://thomas-james-rose.github.io/platform22-site/)
--- js/main.js
+++ js/main.js
@@ -1,16 +1,13 @@
-// on scroll (source: http://stackoverflow.com/questions/28392145/change-css-after-scrolling)
 $(window).on('scroll',function(){
-
-    var headerbottom = $('#page-header').offset().top + $('#page-header').height();
-
-    // we round here to reduce a little workload
-    stop = Math.round($(window).scrollTop());
-    if (stop > headerbottom) {
+    if (Math.round($(window).scrollTop()) > getElementBottom('page-header')) {
         $('#main-nav').addClass('topnav');
         $('#top-arrow').addClass('top-arrow-active')
     } else {
         $('#main-nav').removeClass('topnav');
         $('#top-arrow').removeClass('top-arrow-active')
     }
-
 });
+
+function getElementBottom(id) {
+    return $('#' + id).offset().top + $('#' + id).height();
+}
